b7ce5c34181be4220f0cbac2a0c7167f64441daa
Felipejenoy <johanordonez@unicauca.edu.co>
2025-08-06 18:32:52 -0500

    Se creo un archivo nuevo para aprender a usar el for, recorri una matriz

---
+++ arrrayFrutas.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+            $frutas =array("Mora","fresa","guayaba","manzana");
+
+            for ($i=0; $i < count($frutas); $i++) { 
+                echo $frutas [$i];
+            }
+        ?>
+</body>
+</html>
