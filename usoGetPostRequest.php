80300f27b5966218df03d9a648ad8bbc7d384a08
Felipe Ordoñez <johanordonez@unicauca.edu.co>
2025-08-13 22:42:57 -0500

    Se sube el código completo hasta la fecha de hoy

---
+++ usoGetPostRequest.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>use of get, post and request</title>
+</head>
+<body>
+    <?php
+        
+    ?>
+    <form method="post" action="funciones.php">
+    name: <input type="text"name= "fname">
+    <input type="submit">
+    </form>
+    
+</body>
+</html>
