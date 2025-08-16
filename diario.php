48f09c8decaab7973ef2316d5533756a3f56c1d1
Felipe Ordoñez <johanordonez@unicauca.edu.co>
2025-08-15 21:49:57 -0500

    se creo un archivo de practica

---
+++ diario.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi diario</title>
+</head>
+<body>
+    <form action="saludarconPost.php" method="post">
+
+        nombre: <input type="text" name="nombre"><br><br>
+        <input type="submit">
+    </form>
+</body>
+</html>
