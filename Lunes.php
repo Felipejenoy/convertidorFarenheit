2828db10c7424aeac6051331b64d70c5481ecb09
Felipe Ordoñez <johanordonez@unicauca.edu.co>
2025-08-18 20:08:38 -0500

    se creo un archivo de practica

---
+++ Lunes.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="<?php echo $_SERVER["PHP_SELF"]?>" method="post">
+        Nombre: <input type="text" name="Hola"><br>
+        <input type="submit">
+    </form>
+    hola <?php echo $_POST["Hola"]?>
+</body>
+</html>
